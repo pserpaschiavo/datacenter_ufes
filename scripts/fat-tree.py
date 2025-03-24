a83d259521659a765fab48252bfe57a71ce6198f
Phil Serpa Schiavo <pserpaschiavo@gmail.com>
2025-03-24 12:23:00 -0300

    feat: add a VM ready for use

--- scripts/fat-tree.py
+++ scripts/fat-tree.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.node import RemoteController
@@ -8,49 +9,57 @@ class MyTopo(Topo):
     def __init__(self):
         super(MyTopo, self).__init__()
 
-        # Number of switches per layer
+        # Número de switches por camada
         k = 4
         pod = k
         L1 = (pod // 2) ** 2
         L2 = pod * pod // 2
         L3 = L2
 
-        # Creating switches
-        c = []  # core layer switches
-        a = []  # aggregation layer switches
-        e = []  # edge layer switches
+        # Criação dos switches
+        c = []  # switches do Core
+        a = []  # switches de Agregação
+        e = []  # switches de Borda (Edge)
 
-        # Add switches
+        # Adicionar switches do Core
         for i in range(L1):
             c_sw = self.addSwitch('c{}'.format(i + 1))
             c.append(c_sw)
 
+        # Adicionar switches de Agregação
         for i in range(L2):
             a_sw = self.addSwitch('a{}'.format(L1 + i + 1))
             a.append(a_sw)
 
+        # Adicionar switches de Borda
         for i in range(L3):
             e_sw = self.addSwitch('e{}'.format(L1 + L2 + i + 1))
             e.append(e_sw)
 
-        # Creating links between switches
-        # The core layer and aggregation layer links
+        # Criar links entre os switches
+        # Links entre Core e Agregação
         for i in range(L1):
             c_sw = c[i]
             start = i % (pod // 2)
             for j in range(pod):
                 self.addLink(c_sw, a[start + j * (pod // 2)], bw=10)
 
-        # aggregation and edge layer links
+        # Links entre Agregação e Borda
         for i in range(L2):
             group = i // (pod // 2)
             for j in range(pod // 2):
                 self.addLink(a[i], e[group * (pod // 2) + j], bw=10)
 
-        # Creating hosts and adding links between switches and hosts
+        # Criar hosts e ligar aos switches de Borda
+        # Atribuindo o IP diretamente na criação para evitar problemas posteriores.
         for i in range(L3):
             for j in range(2):
-                hs = self.addHost('h{}'.format(i * 2 + j + 1), ip=None, bw=10)
+                ip_address = "10.2.{}.{}/24".format(i + 1, j + 2)
+                default_route = "via 10.2.{}.1".format(i + 1)
+                hs = self.addHost('h{}'.format(i * 2 + j + 1),
+                                  ip=ip_address,
+                                  defaultRoute=default_route,
+                                  bw=10)
                 self.addLink(e[i], hs)
 
 topos = {"mytopo": (lambda: MyTopo())}
@@ -62,7 +71,7 @@ def configure_ips(net, topo):
     L2 = pod * pod // 2
     L3 = L2
 
-    # Configurar IPs dos switches
+    # Configurar IPs dos switches (mantendo a lógica já definida)
     for i in range(L1):
         switch = net.getNodeByName('c{}'.format(i + 1))
         switch.cmd('ip addr add 10.0.{}.1/24 dev c{}-eth1'.format(i + 1, i + 1))
@@ -79,7 +88,7 @@ def configure_ips(net, topo):
         switch.cmd('ip addr add 10.1.{}.1/24 dev a{}-eth1'.format(i + 1, L1 + i + 1))
         switch.cmd('ip link set a{}-eth1 up'.format(L1 + i + 1))
         switch.cmd('ip addr add 10.1.{}.2/24 dev a{}-eth2'.format(i + 1, L1 + i + 1))
-        switch.cmd('ip link set a{}-eth2 up'.format(L1 + i + 1))  # Corrigido aqui
+        switch.cmd('ip link set a{}-eth2 up'.format(L1 + i + 1))
         switch.cmd('ip addr add 10.1.{}.3/24 dev a{}-eth3'.format(i + 1, L1 + i + 1))
         switch.cmd('ip link set a{}-eth3 up'.format(L1 + i + 1))
         switch.cmd('ip addr add 10.1.{}.4/24 dev a{}-eth4'.format(i + 1, L1 + i + 1))
@@ -96,22 +105,14 @@ def configure_ips(net, topo):
         switch.cmd('ip addr add 10.2.{}.4/24 dev e{}-eth4'.format(i + 1, L1 + L2 + i + 1))
         switch.cmd('ip link set e{}-eth4 up'.format(L1 + L2 + i + 1))
 
-    # Configurar IPs dos hosts
-    for i in range(L3):
-        for j in range(2):
-            ip_address = "10.2.{}.{}/24".format(i + 1, j + 2)
-            # Aqui, atribuímos o IP e também podemos definir a rota padrão, se desejar
-            hs = self.addHost('h{}'.format(i * 2 + j + 1), ip=ip_address, defaultRoute="via 10.2.{}.1".format(i + 1), bw=10)
-            self.addLink(e[i], hs)
-
-    # Instalar FRR e traceroute e iniciar FRR
+    # Instalar FRR e traceroute nos switches
     for switch in net.switches:
         switch.cmd('apt-get update')
         switch.cmd('apt-get install -y traceroute frr')
         switch.cmd('frr -d -l stdout -f frr_configs/frr_{}.conf'.format(switch.name))
         switch.cmd('service frr status')
 
-    # Instala o traceroute e adiciona os servidores DNS nos hosts
+    # Configurar DNS, atualizar e instalar traceroute nos hosts
     for host in net.hosts:
         host.cmd('echo "nameserver 8.8.8.8" >> /etc/resolv.conf')
         host.cmd('echo "nameserver 8.8.4.4" >> /etc/resolv.conf')
