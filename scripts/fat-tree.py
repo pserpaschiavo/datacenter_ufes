fa8a053ea7f83c64ddf11952a551c4a19bc2c44b
Phil Serpa Schiavo <pserpaschiavo@gmail.com>
2025-03-24 14:20:35 -0300

    fix: fix python scripts

--- scripts/fat-tree.py
+++ scripts/fat-tree.py
@@ -1,126 +1,79 @@
-#!/usr/bin/env python3
 from mininet.topo import Topo
 from mininet.net import Mininet
-from mininet.node import RemoteController, Node
+from mininet.node import RemoteController, Node, OVSKernelSwitch
 from mininet.link import TCLink
 from mininet.util import dumpNetConnections
 
-# Definição de um roteador Linux que ativa o IP forwarding
+# Roteador baseado em Linux
 class LinuxRouter(Node):
     def config(self, **params):
         super(LinuxRouter, self).config(**params)
-        # Ativar o IP forwarding para roteamento
         self.cmd('sysctl -w net.ipv4.ip_forward=1')
+
     def terminate(self):
         self.cmd('sysctl -w net.ipv4.ip_forward=0')
         super(LinuxRouter, self).terminate()
 
-class MyTopo(Topo):
+class FatTreeTopo(Topo):
     def __init__(self):
-        super(MyTopo, self).__init__()
+        super(FatTreeTopo, self).__init__()
 
-        # Definir o valor de k e as quantidades por camada
-        k = 4
-        pod = k
-        L1 = (pod // 2) ** 2     # Número de roteadores Core
-        L2 = pod * pod // 2      # Número de roteadores de Agregação
-        L3 = L2                # Número de roteadores de Borda (Edge)
+        k = 4  # Fator k da Fat-Tree
+        num_core = (k // 2) ** 2
+        num_agg = k * k // 2
+        num_edge = num_agg
+        num_hosts_per_edge = 2
 
-        # Criação dos nós de roteamento (LinuxRouter) para todas as camadas
-        core = []  # Core
-        agg = []   # Agregação
-        edge = []  # Edge
+        core_switches = []
+        agg_switches = []
+        edge_switches = []
+        routers = []
+        hosts = []
 
-        # Adiciona roteadores Core
-        for i in range(L1):
-            r = self.addNode('c{}'.format(i+1), cls=LinuxRouter)
-            core.append(r)
+        # Adiciona switches Core
+        for i in range(num_core):
+            switch = self.addSwitch(f'c{i+1}', cls=OVSKernelSwitch)
+            core_switches.append(switch)
 
-        # Adiciona roteadores de Agregação
-        for i in range(L2):
-            r = self.addNode('a{}'.format(L1+i+1), cls=LinuxRouter)
-            agg.append(r)
+        # Adiciona switches Agregação
+        for i in range(num_agg):
+            switch = self.addSwitch(f'a{i+1}', cls=OVSKernelSwitch)
+            agg_switches.append(switch)
 
-        # Adiciona roteadores Edge
-        for i in range(L3):
-            r = self.addNode('e{}'.format(L1+L2+i+1), cls=LinuxRouter)
-            edge.append(r)
+        # Adiciona switches Edge
+        for i in range(num_edge):
+            switch = self.addSwitch(f'e{i+1}', cls=OVSKernelSwitch)
+            edge_switches.append(switch)
 
-        # Criar links entre Core e Agregação
-        for i in range(L1):
-            core_node = core[i]
-            start = i % (pod // 2)
-            for j in range(pod):
-                agg_node = agg[start + j * (pod // 2)]
-                self.addLink(core_node, agg_node, bw=10)
+        # Adiciona roteadores na camada Edge
+        for i in range(num_edge):
+            router = self.addNode(f'r{i+1}', cls=LinuxRouter)
+            routers.append(router)
+            self.addLink(router, edge_switches[i], bw=10)
 
-        # Criar links entre Agregação e Edge
-        for i in range(L2):
-            group = i // (pod // 2)
-            for j in range(pod // 2):
-                edge_index = group * (pod // 2) + j
-                self.addLink(agg[i], edge[edge_index], bw=10)
+        # Conectar Core a Agregação
+        for i in range(num_core):
+            for j in range(k // 2):
+                self.addLink(core_switches[i], agg_switches[(i % (k // 2)) + j * (k // 2)], bw=10)
 
-        # Criar hosts e ligá-los aos roteadores Edge
-        for i in range(L3):
-            for j in range(2):
-                ip_address = "10.2.{}.{}/24".format(i+1, j+2)
-                default_route = "via 10.2.{}.1".format(i+1)
-                h = self.addHost('h{}'.format(i*2+j+1),
-                                 ip=ip_address,
-                                 defaultRoute=default_route,
-                                 bw=10)
-                self.addLink(edge[i], h, bw=10)
+        # Conectar Agregação a Edge
+        for i in range(num_agg):
+            for j in range(k // 2):
+                self.addLink(agg_switches[i], edge_switches[(i // (k // 2)) * (k // 2) + j], bw=10)
 
-topos = { "mytopo": (lambda: MyTopo()) }
+        # Criar Hosts e conectar aos switches Edge
+        for i in range(num_edge):
+            for j in range(num_hosts_per_edge):
+                host = self.addHost(f'h{i*num_hosts_per_edge + j + 1}', ip=f'10.0.{i}.{j+2}/24', defaultRoute=f'via 10.0.{i}.1')
+                hosts.append(host)
+                self.addLink(edge_switches[i], host, bw=10)
 
-def configure_ips(net, topo):
-    k = 4
-    pod = k
-    L1 = (pod // 2) ** 2
-    L2 = pod * pod // 2
-    L3 = L2
-
-    # Configurar os endereços IP dos roteadores Core
-    for i in range(L1):
-        router = net.getNodeByName('c{}'.format(i+1))
-        # Assumindo que cada roteador core possui 4 interfaces
-        for intf in range(1,5):
-            router.cmd('ip addr add 10.0.{}.{}0/24 dev c{}-eth{}'.format(i+1, intf, i+1, intf))
-            router.cmd('ip link set c{}-eth{} up'.format(i+1, intf))
-
-    # Configurar os roteadores de Agregação
-    for i in range(L2):
-        router = net.getNodeByName('a{}'.format(L1+i+1))
-        for intf in range(1,5):
-            router.cmd('ip addr add 10.1.{}.{}0/24 dev a{}-eth{}'.format(i+1, intf, L1+i+1, intf))
-            router.cmd('ip link set a{}-eth{} up'.format(L1+i+1, intf))
-
-    # Configurar os roteadores Edge
-    for i in range(L3):
-        router = net.getNodeByName('e{}'.format(L1+L2+i+1))
-        for intf in range(1,5):
-            router.cmd('ip addr add 10.2.{}.{}0/24 dev e{}-eth{}'.format(i+1, intf, L1+L2+i+1, intf))
-            router.cmd('ip link set e{}-eth{} up'.format(L1+L2+i+1, intf))
-
-    # Instalar FRR e traceroute nos roteadores
-    for router in net.controllers + net.switches + net.hosts: 
-        # Aqui você pode ajustar se quiser instalar somente nos roteadores
-        router.cmd('apt-get update')
-        router.cmd('apt-get install -y traceroute frr')
-    
-    # Iniciar FRR nos roteadores: para cada nó que seja roteador, inicie o FRR com o arquivo de configuração correspondente
-    for node in net.controllers + net.switches:
-        # Se você tiver arquivos de configuração separados para cada roteador, pode usar:
-        node_name = node.name
-        node.cmd('frr -d -l stdout -f frr_configs/frr_{}.conf'.format(node_name))
-        node.cmd('service frr status')
+topos = {"fattree": (lambda: FatTreeTopo())}
 
 if __name__ == '__main__':
-    topo = MyTopo()
-    net = Mininet(topo=topo, link=TCLink)
+    topo = FatTreeTopo()
+    net = Mininet(topo=topo, link=TCLink, controller=None)
     net.start()
-    configure_ips(net, topo)
     dumpNetConnections(net.hosts)
     net.interact()
     net.stop()
--- scripts/generate-frr-conf.py
+++ scripts/generate-frr-conf.py
@@ -3,20 +3,26 @@ import argparse
 
 def generate_frr_configs(k):
     pod = k
-    max_paths = k // 2  # Define o número de caminhos iguais, ECMP
-    L1 = (pod // 2) ** 2
-    L2 = pod * pod // 2
-    L3 = L2
+    max_paths = k // 2  # ECMP - Número de caminhos iguais
+    L1 = (pod // 2) ** 2  # Core
+    L2 = pod * pod // 2  # Agregação
+    L3 = L2  # Edge
 
-    if not os.path.exists("frr_configs"):
-        os.makedirs("frr_configs")
+    config_dir = "frr_configs"
+    if not os.path.exists(config_dir):
+        os.makedirs(config_dir)
+
+    # Remove arquivos antigos para evitar conflito
+    for file in os.listdir(config_dir):
+        os.remove(os.path.join(config_dir, file))
 
     # Configuração para os switches Core
     for i in range(L1):
         switch_id = i + 1
         router_id = f"{switch_id}.{switch_id}.{switch_id}.{switch_id}"
         network = f"10.0.{switch_id}.0/24"
-        config = f"""router ospf
+        config = f"""!
+router ospf
  ospf router-id {router_id}
  maximum-paths {max_paths}
  network {network} area 0.0.0.0
@@ -24,16 +30,18 @@ def generate_frr_configs(k):
 line vty
 !
 """
-        with open(f"frr_configs/frr_c{switch_id}.conf", "w") as f:
+        with open(f"{config_dir}/frr_c{switch_id}.conf", "w") as f:
             f.write(config)
 
     # Configuração para os switches de Agregação
     for i in range(L2):
         switch_id = L1 + i + 1
         router_id = f"{switch_id}.{switch_id}.{switch_id}.{switch_id}"
-        network1 = f"10.0.{i // (pod // 2) + 1}.0/24"
+        pod_id = i // (pod // 2)  # Identifica a qual pod pertence
+        network1 = f"10.0.{pod_id + 1}.0/24"
         network2 = f"10.1.{i + 1}.0/24"
-        config = f"""router ospf
+        config = f"""!
+router ospf
  ospf router-id {router_id}
  maximum-paths {max_paths}
  network {network1} area 0.0.0.0
@@ -42,16 +50,18 @@ line vty
 line vty
 !
 """
-        with open(f"frr_configs/frr_a{switch_id}.conf", "w") as f:
+        with open(f"{config_dir}/frr_a{switch_id}.conf", "w") as f:
             f.write(config)
 
     # Configuração para os switches de Borda (Edge)
     for i in range(L3):
         switch_id = L1 + L2 + i + 1
         router_id = f"{switch_id}.{switch_id}.{switch_id}.{switch_id}"
-        network1 = f"10.1.{i // (pod // 2) + 1}.0/24"
+        pod_id = i // (pod // 2)
+        network1 = f"10.1.{pod_id + 1}.0/24"
         network2 = f"10.2.{i + 1}.0/24"
-        config = f"""router ospf
+        config = f"""!
+router ospf
  ospf router-id {router_id}
  maximum-paths {max_paths}
  network {network1} area 0.0.0.0
@@ -60,7 +70,7 @@ line vty
 line vty
 !
 """
-        with open(f"frr_configs/frr_e{switch_id}.conf", "w") as f:
+        with open(f"{config_dir}/frr_e{switch_id}.conf", "w") as f:
             f.write(config)
 
 if __name__ == "__main__":
